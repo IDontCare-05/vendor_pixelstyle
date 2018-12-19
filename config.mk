7b967680a89751b09d1e5dad35c70243f13cb701
ElDainosor <eldainosor@gmail.com>
2018-12-19 22:35:39 +0530

    pixelstyle: Add Ink QS Tile Style [3/3]
    Change-Id: I62b80c4f6903b96dd15d4e173c15f69e7ee875dc

--- config.mk
+++ config.mk
@@ -182,7 +182,8 @@ PRODUCT_PACKAGES += \
     QStileNinja \
     QStilePokesign \
     QStileWavey \
-    QStileSquaremedo
+    QStileSquaremedo \
+    QStileInkDrop
 
 ifeq ($(TARGET_GAPPS_ARCH),arm64)
 PRODUCT_PACKAGES += \
