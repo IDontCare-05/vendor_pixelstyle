d4dc5b4389da52ab7c64c0cd97c7de20cc8e713d
sayan7848 <sayan7848@gmail.com>
2019-02-15 12:51:03 +0000

    pixelstyle: QSTiles: Add one more styles [1/3]
     * CircleOutline
    
    Change-Id: I04754b9bfdb33cc5f38dcb6a7a7a2028c43c606e
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- config.mk
+++ config.mk
@@ -189,7 +189,8 @@ PRODUCT_PACKAGES += \
     QStileWavey \
     QStileSquaremedo \
     QStileInkDrop \
-    QStileCookie
+    QStileCookie \
+    QStilesCircleOutline
 
 ifeq ($(TARGET_GAPPS_ARCH),arm64)
 PRODUCT_PACKAGES += \
